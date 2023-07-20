3798287b3b05b4987be92528743ced3aab731490
Natallia <nsh.nataliss2@gmail.com>
2023-07-21 01:39:40 +0300

    Test lesson 2 - 5

---
+++ lesson-02/05.js
@@ -0,0 +1,20 @@
+// Дан объект passport. Создай его копию и поменяй имя с "Petr" на "Ivan", и убедись в консоли, что у объекта passport имя осталось "Petr", а у твоего нового объекта "Ivan".
+//
+//     let passport = {
+//     name: "Petr",
+//     surname: "Petrov",
+// };
+//
+// ❗Для того, чтобы проверить правильность данного задания должно быть выведено 2 console.log
+
+
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+
+const passport2  = Object.assign({}, passport)
+passport2.name = "Ivan"
+
+console.log(passport2.name)
+console.log(passport.name)
