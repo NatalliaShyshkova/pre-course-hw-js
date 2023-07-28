24f84e2369a3c89365011060f2a8cf772f20385e
Natallia <nsh.nataliss2@gmail.com>
2023-07-28 14:30:13 +0300

    Test 01-03-0

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -2,13 +2,13 @@
 let number1 = Math.floor(Math.random() * 100)
 
 if (number1 % 2 == 0) {
-     console.log("Это четное число")
+     console.log(number1 + ":Это четное число")
 }
 if (number1 % 2 !== 0) {
-    console.log("Это нечетное число")
+    console.log(number1 + ":Это нечетное число")
 }
 
-console.log(number1)
+
 
 
 
