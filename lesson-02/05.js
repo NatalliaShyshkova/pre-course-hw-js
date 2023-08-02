e32c21043b36ba789edb1731db1917eb3083acf5
Natallia <nsh.nataliss2@gmail.com>
2023-08-02 17:25:11 +0300

    Test-02-05-02

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -2,11 +2,9 @@ let passport = {
     name: "Petr",
     surname: "Petrov",
 };
-
 let passport2 = {...passport};
 
-passport2.name = "Ivan"
-
+passport2.name = "Ivan";
 
 console.log(passport2)
 console.log(passport)
